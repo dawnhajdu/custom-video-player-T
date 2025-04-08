212f22a31d9eabbf1ee7b43f51edb3c3b8cc0764
Dawn Hajdu <dawn.hajdu@gmail.com>
2025-04-07 15:15:43 -1000

    Update updateProgressBar function, and setVideoProgress function

--- script.js
+++ script.js
@@ -24,12 +24,26 @@ function updatePlayIcon() {
 
 //Update progress bar with timestamp
 function updateProgressBar() {
-    return true;
+    progressBar.value = (video.currentTime/video.duration) * 100;
+
+    //Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10) {
+        mins = '0' + String(mins);
+    }
+    //Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
+
 }
 
 //Set/Update the progress bar when there is a change in timestamp
 function setVideoProgress() {
-    return true;
+    video.currentTime = (+progressBar.value * video.duration) / 100;
 }
 
 //Stop video
